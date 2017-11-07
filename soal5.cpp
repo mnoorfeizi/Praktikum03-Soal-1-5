efbd72dcc157945409eb7e038a9817f57dbce8b9
mnoorfeizi <mnoorfeizi48@gmail.com>
2017-11-07 17:54:24 +0800

    menentukan ukuran

--- soal5.cpp
+++ soal5.cpp
@@ -3,4 +3,8 @@ using namespace std;
 int main(){
 float bb;
 int tb;
+cout<<"||----PROGRAM MENENTUKAN underweight, normal, atau overweight----||"<<endl;
+cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
+cout << "Silahkan masukkan berat badan(kg) : " ;
+cin >> bb;
 }
