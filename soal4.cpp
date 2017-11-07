46edc3a6f5174a64e55b9850a82ae1fb3cd00ce2
mnoorfeizi <mnoorfeizi48@gmail.com>
2017-11-07 17:48:00 +0800

    keterangan

--- soal4.cpp
+++ soal4.cpp
@@ -7,4 +7,18 @@ cout<<"PROGRAM MENENTUKAN JENIS BILANGAN"<<endl;
 cout<<""<<endl;
 cout<<"Input Bilangan : ";cin>>x;
 cout<<""<<endl;	
+if (x < 0)
+	{
+		cout<<"yang anda input adalah bilangan negatif"<<endl;
+	}
+	
+	else if (x>0)
+	{
+		cout<<"yang anda input adalah bilangan positif"<<endl;
+	}
+	else
+	
+		cout<<"yang anda input adalah bilangan nol "<<endl;
+
+	return 0;
 }
