315c9731b53cf4d9b42146cb6a8112d458301916
mnoorfeizi <mnoorfeizi48@gmail.com>
2017-11-07 17:42:01 +0800

    menentukan input

--- soal3.cpp
+++ soal3.cpp
@@ -3,4 +3,8 @@ using namespace std;
 
 int main(){ 
 float jk, le, gtot, gp, gb,ul, jam,pa;
+cout<<"   PROGRAM GAJI BBERSIH KARYAWAN"<<endl;
+cout<<""<<endl;
+cout<<"Lama Kerja Karyawan    : "; cin>> jk;
+cout<<"Gaji Perjam Karyawan   : "; cin>>gp;
 }
