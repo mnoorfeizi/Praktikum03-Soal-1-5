0a4fb1e82a6756255ba40a54b9e17ef54ed267e6
mnoorfeizi <mnoorfeizi48@gmail.com>
2017-11-07 17:55:24 +0800

    menampilkan keterangan

--- soal5.cpp
+++ soal5.cpp
@@ -7,4 +7,13 @@ cout<<"||----PROGRAM MENENTUKAN underweight, normal, atau overweight----||"<<end
 cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
 cout << "Silahkan masukkan berat badan(kg) : " ;
 cin >> bb;
-}
+if (bb<tb/2.5)
+       cout << "  Anda Mengalami : Underweight(kekurangan berat badan)" << endl;
+            else if (tb/2.3 < bb)
+                 cout << "  Anda Mengalami : Overweight(kelebihan berat badan)" << endl;
+            else if (tb/2.5 <= bb <= tb/2.3)
+           		 cout << "  congratulation!!! : Normal" << endl;
+                 
+                 
+                 return 0;
+    }
